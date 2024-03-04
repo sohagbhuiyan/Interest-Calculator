e66382feabd9061b3e774e272b7bcc85eab1cc50
sohagbhuiyan <smsohag171@gmail.com>
2024-03-05 00:49:31 +0600

    Interest Calculator

---
+++ interest.js
@@ -0,0 +1,33 @@
+//const buton=document.getElementById('buton');
+const totalAmount = document.getElementById('total-amount');
+const princpleInput = document.getElementById('principal');
+const rateIn = document.getElementById('rate');
+const yearsIn = document.getElementById('time');
+///buton.addEventListener('click',()=>{
+function cal(a){
+    console.log(a)
+    let principal = Number( princpleInput.value);
+    let rate = Number(rateIn.value/100);
+    let years = Number(yearsIn.value);
+    
+    if(principal < 0 || isNaN(principal)){
+        principal = 0;
+        princpleInput.value=0;
+    }
+    if(rate < 0 || isNaN (rate)){
+        rate = 0;
+        rateIn.value = 0;
+    }
+    if(years < 0 || isNaN (years)){
+        years = 0;
+        yearsIn.value = 0;
+    }
+    const result = principal* rate* years;
+
+    totalAmount.textContent=result.toLocaleString("bn-BD", {
+        style:"currency",
+        currency: "BDT"
+});
+
+//})
+    }
